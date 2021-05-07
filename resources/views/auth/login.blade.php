2f4aba95bf385b0b0a5bc24ce1cf8caa1a4f0baa
Miguel Avilés <miguelernestoag@gmail.com>
2021-05-07 12:59:23 -0600

    Diseño de login opcional terminado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,6 +16,7 @@
         background-size: 107px;
         width: 100%;
         height: 100%;
+        overflow: hidden;
 
     }
     .cst-card{
@@ -23,12 +24,13 @@
         padding: 42px !important;
         opacity: 0.9;
         box-shadow: 0 0.5rem 1rem rgb(0 0 0 / 50%) !important;
-        background-color: rgb(0 0 0 / 40%);
+        background-color: white;
         display: flex;
         flex-wrap: wrap;
         margin: 20px;
         justify-content: center;
         align-items: center;
+        flex-direction: column;
     }
 
     .cst-container{
@@ -41,14 +43,14 @@
 
     .cst-input{
         width: 100%;
-        color: white;
+        color: black;
         margin-bottom: 14px;
         font-size: 14px;
         height: 35px;
         border: 4px solid black;
         background-color: #efe5e500;
         border-color: transparent !important;
-        border-bottom-color: #4ac04a !important;
+        border-bottom-color: #305ae9 !important;
         font-weight: bold;
 
     }
@@ -62,7 +64,7 @@
       left: 0px;
       opacity: 1;
       font-weight: bolder !important;
-      color:white;
+      color:black;
   }
 
   .cst-input:focus {
@@ -94,6 +96,8 @@
     border-radius: 50px;
     font-size: 1.2rem;
     transition: 1s ease all;
+    width: 70%;
+    font-size: 1rem;
 }
 
 .cst-btn:hover{
@@ -112,12 +116,32 @@
 
 }
 
+.cst-img{
+    width: 50%;
+}
+
+.cst-img-container{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+}
+.cst-btn-cont{
+    display: flex;
+    width: 100%;
+    justify-content: center;
+}
+
 input:-webkit-autofill,
 input:-webkit-autofill:hover,
 input:-webkit-autofill:focus,
 input:-webkit-autofill:active {
     transition: background-color 5000s ease-in-out 0s;
-    color: white;
+    color: black;
+}
+
+.floating-label{
+    color: black;
+    text-transform: uppercase;
 }
 </style>
 </head>
@@ -126,7 +150,7 @@ input:-webkit-autofill:active {
 
         <div class="cst-card">
             <div class="cst-img-container">
-                <img src="/img/.svg" alt="AVATAR" class="w-48  self-center">
+                <img src="/img/icon/logo_iudop.png" alt="" class="w-48  self-center cst-img">
             </div>
             <form method="POST" action="{{ route('login') }}" class="cst-form">
                 {{ csrf_field() }}
@@ -149,7 +173,7 @@ input:-webkit-autofill:active {
                 @endforeach
                 @endif
                 <div class="form-group row mb-0">
-                    <div class="col-md-8 offset-md-4">
+                    <div class="cst-btn-cont">
                         <button type="submit" class="no-outline-focus cst-btn">
                             INICIAR SESIÓN
                         </button>
