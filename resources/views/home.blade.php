3a44be69bbb810e384fda6be09c56feccdedda41
Miguel Avilés <miguelernestoag@gmail.com>
2021-05-09 16:03:13 -0600

    Header,container,footer identificados

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
+<div class="container cst-container">
     <div>
         CONTENIDO
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,16 +4,38 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style type="text/css" media="screen">
+        body{
+            width: 100%;
+            margin: 0 !important;
+            height: 100vh;
+        }
 
+        .cst-navbar{
+            height: 10vh !important;
+            background-color: cornflowerblue;
+        }
+
+        .cst-footer{
+            height: 10vh !important;
+            background-color: cornflowerblue;
+        }
+
+        .cst-container{
+            background-color: gray;
+            height: 80vh !important;
+        }
+
+    </style>
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
+        <nav class="navbar navbar-default navbar-static-top cst-navbar">
             HEADER
         </nav>
 
         @yield('content')
-        <footer>
+        <footer class="cst-footer">
             FOOTER
         </footer>
     </div>
